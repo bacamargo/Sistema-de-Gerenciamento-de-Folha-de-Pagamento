bac39e78459d7fb64250d8ec199e54dab9248068
JulianaDantass <julianadantas498@gmail.com>
2022-06-02 01:59:07 -0300

    up

---
+++ Code/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "gerenciamento.hpp"
+#include "funcionario.hpp"
+#include "diretor.hpp"
+#include "gerente.hpp"
+#include "operador.hpp"
+#include "presidente.hpp"
+
+using namespace std;
+ 
+
+int main(){
+
+
+
+
+
+    return 0;
+}
